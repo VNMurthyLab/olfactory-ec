d3c11f43d48a57f8296e7d9a1eb385e507f8abef
Juan Carlos Fernandez del Castillo <jfernandezdelcasti@boslogin05.rc.fas.harvard.edu>
2025-09-09 10:16:22 -0400

    remove script for cleaning paths

--- clean_up_paths_for_publication.sh
+++
@@ -1,5 +0,0 @@
-# Replace ALL absolute paths with simple placeholders
-find sample_generation/configs_flat_frequencies/ -name "*.json" -exec sed -i '' \
-  -e 's|/n/home10/jfernandezdelcasti/noncanonical-olfaction/model/results/|{{RESULTS_DIR}}/|g' \
-  -e 's|/n/holylabs/LABS/murthy_users/Lab/juancarlos/efficient_coding_olfaction/|{{DATA_DIR}}/|g' \
-  {} \;
